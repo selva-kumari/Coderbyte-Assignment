72befdf0b1f171028942a67ca5730de1e1b1b3b7
selva-kumari <113326793+selva-kumari@users.noreply.github.com>
2023-08-27 09:56:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package FXTrade;
-public class Main {
+public class Main extends TradeDetails{
 	public static void main(String[] args){
+		TradeProcess tradeprocess=new TradeProcess();
 		System.out.println("Welcome to Foreign Exchange Trading");
 		while(true)
 		{
-			TradeProcess.displayOptions();
+			tradeprocess.displayOptions();
 		}
 	}
 }
